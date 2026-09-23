6ecda16635ff7bafee95841af640cd7b9a29bfa2
Nudyess <to.czyzewski@gmail.com>
2026-09-23 19:39:25 +0200

    added new feature to the pre order block

--- assets/product.js
+++ assets/product.js
@@ -191,7 +191,7 @@ if (!customElements.get('variant-selects')) {
     }
 
     getSectionsToRender() {
-      return [`price-${this.dataset.section}`, `price-${this.dataset.section}--sticky`, `product-image-${this.dataset.section}--sticky`, `inventory-${this.dataset.section}`, `sku-${this.dataset.section}`, `quantity-${this.dataset.section}`];
+      return [`price-${this.dataset.section}`, `price-${this.dataset.section}--sticky`, `product-image-${this.dataset.section}--sticky`, `inventory-${this.dataset.section}`, `sku-${this.dataset.section}`, `quantity-${this.dataset.section}`, `pre-order-note-${this.dataset.section}`];
     }
 
     getSelectedValueIds() {
